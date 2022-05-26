80863292166a4d23fb68517b9bc66a43ba3c6e83
Chi-Chang Lee <r08922a27@csie.ntu.edu.tw>
2022-05-26 19:42:21 +0800

    update

--- generate_enhanced_results.sh
+++ generate_enhanced_results.sh
@@ -1,11 +1,11 @@
-chime_root=/CHiME4/data/audio/16kHz/isolated_1ch_track
+chime_root=../CHiME4/data/audio/16kHz/isolated_1ch_track
 python main.py --task write --test_root $chime_root --method NOIS --test_set chime --output_dir $chime_root
-for method in INIT CLSO GCLB D4AM; do
+for method in INIT CLSO SRPR GCLB D4AM; do
     python main.py --task write --test_root $chime_root --ckpt ckpt/$method.pth --output_dir results/${method}_chime_result --bsz 8 --method $method --test_set chime
 done
-aurora_root=/Aurora4
+aurora_root=../Aurora4_eval
 python main.py --task write --test_root $aurora_root --method NOIS --test_set aurora --output_dir $aurora_root
-for method in INIT CLSO GCLB D4AM; do
+for method in INIT CLSO SRPR GCLB D4AM; do
     python main.py --task write --test_root $aurora_root --ckpt ckpt/$method.pth --output_dir results/${method}_aurora_result --bsz 8 --method $method --test_set aurora
 done
 
